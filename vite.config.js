106c14eea3d2ec70d8c01858da0cd1dfa75dbb08
cesardmn <cesardmn@gmail.com>
2025-05-23 06:35:47 -0300

    chore: adiciona arquivos iniciais do projeto

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0]
+          }
+        },
+      },
+    },
+  },
+}

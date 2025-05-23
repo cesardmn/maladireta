3edbbb9dcb1f6145aa43f4f70665792c8b0c02cb
cesardmn <cesardmn@gmail.com>
2025-05-23 16:49:47 -0300

    feat: cria validador de arquivos

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 const App = (() => {
-
   const setFooter = (() => {
     const date = new Date().getFullYear()
     const footer = document.querySelector('footer')
     footer.innerText = `© autoflux ${date} - Cesar Dimi`
   })()
 
+  const isValidFile = (file, type) => {
+    if (!file || !file.name) return false
+    const fileName = file.name.toLowerCase()
+    const expectedType = type.toLowerCase()
+    return fileName.endsWith(expectedType)
+  }
 })()

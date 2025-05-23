4d4e8b83f6b715c7e010db583efd2b162298951d
cesardmn <cesardmn@gmail.com>
2025-05-23 16:56:23 -0300

    feat: adiciona listener de importação para documentos .docx

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,22 @@ const App = (() => {
     footer.innerText = `© autoflux ${date} - Cesar Dimi`
   })()
 
+  // ELEMENTS
+  const $docxInput = document.querySelector('#docxInput')
+
+  // VALIDATORS
   const isValidFile = (file, type) => {
     if (!file || !file.name) return false
     const fileName = file.name.toLowerCase()
     const expectedType = type.toLowerCase()
     return fileName.endsWith(expectedType)
+  }
+
+  // LISTENERS
+  $docxInput.onchange = (e) => {
+    const file = e.target.files[0]
+    const isValid = isValidFile(file, '.docx')
+    console.log(isValid)
+    e.target.value = ''
   }
 })()

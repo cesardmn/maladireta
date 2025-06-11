1d23bc4bbe64874b42e35ba58988f2ba4c8b5342
cesardmn <cesardmn@gmail.com>
2025-06-11 13:45:41 -0300

    core: setup inicial.

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <main >
+      <h1>mala direta</h1>
+    </main>
+    
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    host: true,
+  },
   build: {
     rollupOptions: {
       output: {
@@ -10,4 +20,8 @@ export default {
       },
     },
   },
-}
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
